1c288fc788cfaaabd23fa9ecc2351b2a23e92bdc
alamin hossain <mh1134938@gmail.com>
2022-04-05 08:57:37 +0600

    all part setup commit

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -17,9 +17,6 @@ const Blogs = () => {
             the developer. <br /> Examples of non-semantic elements: div and span -
             Tells nothing about its content. <br /> Semantic elements: header, main,
             footer, form, article - Clearly define its content. <br /> HTML5 introduces
-            many semantic elements as mentioned below which make the code easier
-            to write and understand for the developer as well as instructs the
-            browser on how to treat them.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
@@ -36,9 +33,6 @@ const Blogs = () => {
             produce global variables that can be passed around. This is the
             alternative to "prop drilling" or moving props from grandparent to
             child to parent, and so on. <br /> Context Api, allows us to pass data through our component trees,
-              giving our components the ability to communicate and share data at
-              different levels. <br /> Context is also touted as an easier,
-            lighter approach to state management using Redux.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,21 +5,21 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <div className='container navbar justify-content-center'>
-            <NavLink 
-            className={({isActive}) => (isActive? 'active-link' : 'link')} 
-            to='/home'>Home</NavLink>
-            <NavLink 
-            className={({isActive}) => (isActive? 'active-link' : 'link')} 
-            to='/reviews'>Reviews</NavLink>
-            <NavLink 
-            className={({isActive}) => (isActive? 'active-link' : 'link')} 
-            to='/dashboard'>Dashboard</NavLink>
-            <NavLink 
-            className={({isActive}) => (isActive? 'active-link' : 'link')} 
-            to='/blogs'>Blogs</NavLink>
-            <NavLink 
-            className={({isActive}) => (isActive? 'active-link' : 'link')} 
-            to='/about'>About</NavLink>
+            <NavLink
+                className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+                to='/home'>Home</NavLink>
+            <NavLink
+                className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+                to='/reviews'>Reviews</NavLink>
+            <NavLink
+                className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+                to='/dashboard'>Dashboard</NavLink>
+            <NavLink
+                className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+                to='/blogs'>Blogs</NavLink>
+            <NavLink
+                className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+                to='/about'>About</NavLink>
         </div>
     );
 };
